92e12d94a6c7619f75d466058dce2e72f69eef43
Janice Kaur <59028457+JaniceKaur@users.noreply.github.com>
2022-01-12 02:54:43 +0530

    Added solution - LeetHub

---
+++ Sum of elements between k1'th and k2'th smallest elements - GFG/sum-of-elements-between-k1th-and-k2th-smallest-elements.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+
+class Solution:
+    def sumBetweenTwoKth(self, A, N, K1, K2):
+        A.sort()
+        sum=0
+        for i in range(K1,K2-1):
+            sum+=A[i]
+        return sum 
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        sz = [int(x) for x in input().strip().split()]
+        k1, k2 = sz[0], sz[1]
+        ob=Solution()
+        print( ob.sumBetweenTwoKth(a, n, k1, k2) )
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+# } Driver Code Ends
