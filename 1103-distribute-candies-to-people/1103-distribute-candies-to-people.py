21f7312617b2bfbd312465004c2ea8c99fa18735
Janice Kaur <59028457+JaniceKaur@users.noreply.github.com>
2022-01-11 17:45:53 +0530

    arrays

--- 1103-distribute-candies-to-people/1103-distribute-candies-to-people.py
+++ 1103-distribute-candies-to-people/1103-distribute-candies-to-people.py
@@ -2,7 +2,7 @@ class Solution:
     def distributeCandies(self, candies: int, num_people: int) -> List[int]:
         arr=[0]*num_people
         count=1
-        while(candies>=0):
+        while(candies>=0): 
             for i in range(num_people):
                 if candies>count:
                     arr[i]+=count
