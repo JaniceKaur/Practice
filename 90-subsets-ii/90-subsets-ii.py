50a7219ec4a25bffe36bff98e26a660292683ace
Janice Kaur <59028457+JaniceKaur@users.noreply.github.com>
2022-01-18 22:15:07 +0530

    Time: 114 ms (5.33%), Space: 14.5 MB (58.06%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subset(self, idx,nums,l1=list()):
+        if(idx>=len(nums)):
+            if(l1 not in self.l):
+                self.l.append(l1)
+                return 
+            else:
+                return
+        l1.append(nums[idx])
+        self.subset(idx+1,nums,copy.deepcopy(l1))
+        l1.remove(nums[idx])
+        self.subset(idx+1,nums,copy.deepcopy(l1))
+    
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        self.l=[]
+        idx=0
+        self.subset(idx,nums)
+        return self.l
