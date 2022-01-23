e821ad054dd6e11cbb4d8c5c101c5dad1ada08bc
Janice Kaur <59028457+JaniceKaur@users.noreply.github.com>
2022-01-24 00:53:44 +0530

    Time: 4 ms (67.53%), Space: 7.6 MB (68.28%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -13,12 +13,14 @@ public:
                 else if(i==0 && j==0)
                     grid[0][0]=1;
                 else
-                {int up=0,left=0;
-                if(i>0)
-                    up=grid[i-1][j];
-                if(j>0)
-                    left=grid[i][j-1];
-                grid[i][j]=up+left;}
+                {
+                    int up=0,left=0;
+                    if(i>0)
+                        up=grid[i-1][j];
+                    if(j>0)
+                        left=grid[i][j-1];
+                    grid[i][j]=up+left;
+                }
                 
             }
         return grid[m-1][n-1];
