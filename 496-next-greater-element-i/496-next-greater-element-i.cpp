93a71a85d94dd5d7b20161778ab78b6da5920b59
Janice Kaur <59028457+JaniceKaur@users.noreply.github.com>
2022-07-27 13:40:21 +0530

    Time: 4 ms (90.39%), Space: 9 MB (33.23%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -5,23 +5,20 @@ public:
         int n2=nums2.size();
         unordered_map<int,int> map;
         stack<int> s;
-        vector<int> result;
-        for(int i=n2-1;i>=0;i--)
-        {
+        vector<int> vec;
+    
+        for(int i=n2-1;i>=0;i--){
             while(!s.empty() && nums2[i]>=s.top())
                 s.pop();
-            
             if(s.empty())
                 map[nums2[i]]=-1;
             else
                 map[nums2[i]]=s.top();
-            
             s.push(nums2[i]);
         }
-        for(int i=0;i<n1;i++)
-        {
-            result.push_back(map[nums1[i]]);
+        for(int i=0;i<n1;i++){
+            vec.push_back(map[nums1[i]]);
         }
-        return result;
+        return vec;
     }
 };
