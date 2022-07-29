d26d7155506b655552b2aad55ceb4573b2ef08e5
Janice Kaur <59028457+JaniceKaur@users.noreply.github.com>
2022-07-29 15:51:49 +0530

    Time: 16 ms (88.69%), Space: 11.5 MB (29.90%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -8,14 +8,15 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// O(1) Space Complexity soln.
+// Iterative Solution
 class Solution {
 public:
     void reverse(ListNode* head,ListNode* end){
         ListNode*p=NULL;
         ListNode*c =head;
-        ListNode*n=head->next;
         while(p!=end){
-             
             ListNode* temp=c->next;
             c->next=p;
             p=c;
@@ -26,15 +27,28 @@ public:
     ListNode* reverseKGroup(ListNode* head, int k) {
         if(head==NULL||head->next==NULL||k==1)
             return head;
-        ListNode *s=head,*e=head;
-        for(int i=0;i<k-1;i++){
-            e=e->next;
-            if(e==NULL)
-                return s;
+        ListNode* temp=new ListNode(-1);
+        temp->next=head;
+        ListNode *e=head,*dummy=temp;
+        int i=0;
+        while(e!=NULL){
+            i++;
+            if(i%k==0){
+                
+                ListNode* s=dummy->next,*e_next=e->next;
+                reverse(s,e);
+                dummy->next=e;
+                s->next=e_next;
+                dummy=s;
+                e=e_next;
+                
+            }
+            else
+            {
+                e=e->next;
+            }
+            
         }
-        ListNode* next_head=reverseKGroup(e->next,k);
-        reverse(s,e);
-        s->next=next_head;
-        return e;
+        return temp->next;
     }
 };
